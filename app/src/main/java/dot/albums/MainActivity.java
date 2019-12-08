b9d98642323de83ae28e0da09a5f945e5b38e65a
hasaansworld <hasaanahmed.pk@gmail.com>
2019-12-09 03:05:14 +0500

    designed main screen

--- app/src/main/java/dot/albums/MainActivity.java
+++ app/src/main/java/dot/albums/MainActivity.java
@@ -1,14 +1,33 @@
 package dot.albums;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.CustomTarget;
+import com.bumptech.glide.request.transition.Transition;
 
 public class MainActivity extends AppCompatActivity {
 
     SharedPreferences sharedPreferences;
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+    MainAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +36,66 @@ public class MainActivity extends AppCompatActivity {
 
         sharedPreferences = getSharedPreferences("data", MODE_PRIVATE);
 
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        setProfilePicture();
 
+        recyclerView = findViewById(R.id.recyclerView);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2);
+        gridLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                if(position == 0)
+                    return 2;
+                else
+                    return 1;
+            }
+        });
+        recyclerView.setLayoutManager(gridLayoutManager);
+        adapter = new MainAdapter(this);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    public void setProfilePicture(){
+        if(getSupportActionBar() != null){
+            String path = sharedPreferences.getString("profilePicPath", "");
+            final int dpToPx = Math.round(dpToPixel(30, this));
+            Glide.with(this)
+                    .load(path)
+                    .override(dpToPx, dpToPx)
+                    .apply(RequestOptions.circleCropTransform())
+                    .diskCacheStrategy(DiskCacheStrategy.NONE)
+                    .placeholder(R.drawable.profile_picture)
+                    .into(new CustomTarget<Drawable>() {
+                        @Override
+                        public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+                            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+                            getSupportActionBar().setHomeAsUpIndicator(resource);
+                        }
+
+                        @Override
+                        public void onLoadCleared(@Nullable Drawable placeholder) {
+
+                        }
+                    });
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static float dpToPixel(float dp, Context context){
+        return dp * ((float) context.getResources().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT);
     }
 
 }
