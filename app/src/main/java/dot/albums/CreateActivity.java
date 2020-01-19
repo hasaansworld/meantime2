68476146d7a7a231594aea03e4a7089bf1befc57
hasaansworld <hasaanahmed.pk@gmail.com>
2020-01-19 07:24:40 +0500

    designed new reminder screen

--- app/src/main/java/dot/albums/CreateActivity.java
+++ app/src/main/java/dot/albums/CreateActivity.java
@@ -2,12 +2,23 @@ package dot.albums;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.vanniktech.emoji.EmojiEditText;
+import com.vanniktech.emoji.EmojiPopup;
+import com.vanniktech.emoji.listeners.OnEmojiPopupDismissListener;
 
 public class CreateActivity extends AppCompatActivity {
+    CoordinatorLayout root;
     Toolbar toolbar;
+    ImageView emojiTitle, emojiDescription;
+    EmojiEditText title, description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,13 @@ public class CreateActivity extends AppCompatActivity {
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_arrow_back_black_24dp);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+        root = findViewById(R.id.root);
+        title = findViewById(R.id.title);
+        description = findViewById(R.id.description);
+        emojiTitle = findViewById(R.id.emojiTitle);
+        emojiDescription = findViewById(R.id.emojiDescription);
+        initializeEmoji(emojiTitle, title);
+        initializeEmoji(emojiDescription, description);
     }
 
     @Override
@@ -28,5 +46,21 @@ public class CreateActivity extends AppCompatActivity {
             finish();
         }
         return true;
+    }
+
+    public void initializeEmoji(final ImageView imageView, EmojiEditText emojiEditText){
+        final EmojiPopup emojiPopup = EmojiPopup.Builder.fromRootView(root).setOnEmojiPopupDismissListener(new OnEmojiPopupDismissListener() {
+            @Override
+            public void onEmojiPopupDismiss() {
+                imageView.setImageResource(R.drawable.outline_sentiment_satisfied_black_24);
+            }
+        }).build(emojiEditText);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setImageResource(emojiPopup.isShowing() ? R.drawable.outline_sentiment_satisfied_black_24 : R.drawable.ic_keyboard_black_24dp);
+                emojiPopup.toggle();
+            }
+        });
     }
 }
--- app/src/main/java/dot/albums/DotApplication.java
+++ app/src/main/java/dot/albums/DotApplication.java
@@ -2,10 +2,14 @@ package dot.albums;
 
 import android.app.Application;
 
+import com.vanniktech.emoji.EmojiManager;
+import com.vanniktech.emoji.ios.IosEmojiProvider;
+
 public class DotApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
         RealmUtils.init(this);
+        EmojiManager.install(new IosEmojiProvider());
     }
 }
