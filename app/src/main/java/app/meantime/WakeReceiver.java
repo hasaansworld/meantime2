101ccc00da3e27ab799cd93fb9aacca890001b66
hasaansworld <hasaanahmed.pk@gmail.com>
2020-04-07 01:02:15 +0500

    schedule reminders after restart

--- app/src/main/java/app/meantime/WakeReceiver.java
+++ app/src/main/java/app/meantime/WakeReceiver.java
@@ -1,17 +1,58 @@
 package app.meantime;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import io.realm.Realm;
+
 public class WakeReceiver extends BroadcastReceiver {
+    AlarmManager alarmManager;
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        Realm realm = Realm.getDefaultInstance();
+        alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+
+        List<DataReminder> scheduledReminders = new ArrayList<>();
+        scheduledReminders.addAll(realm.where(DataReminder.class).equalTo("status", DataReminder.STATUS_SCHEDULED).findAll());
+
+        for(DataReminder reminder: scheduledReminders)
+            scheduleReminder(context, reminder);
+
+
+        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             context.startForegroundService(new Intent(context, ForegroundService.class));
         } else {
             context.startService(new Intent(context, BackgroundService.class));
+        }*/
+    }
+
+    private void scheduleReminder(Context context, DataReminder reminder){
+        Intent intent1 = new Intent(context.getApplicationContext(), NotificationReceiver.class);
+        intent1.setAction(NotificationReceiver.ACTION_NOTIFICATION);
+        intent1.putExtra("id", reminder.getReminderId());
+        int requestCode = reminder.getReminderNumber();
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context.getApplicationContext(), requestCode, intent1,
+                0);
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy");
+            Date date = sdf.parse(reminder.getDate());
+            long timeInMillis = date.getTime();
+            if (Build.VERSION.SDK_INT >= 19)
+                alarmManager.setExact(AlarmManager.RTC, timeInMillis, pendingIntent);
+            else
+                alarmManager.set(AlarmManager.RTC, timeInMillis, pendingIntent);
+        } catch (ParseException e){
+
         }
     }
 }
